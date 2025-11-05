7c7906f7541d5f71cdb5cc0fa79fe075bc3d6ddb
Kucaimon <108683326+Kucaimon@users.noreply.github.com>
2025-11-05 12:34:12 +0300

    справлены пути в finnpower-products.js

--- js/components/finnpower-products.js
+++ js/components/finnpower-products.js
@@ -6,7 +6,7 @@ const finnpowerProductsData = [
     description:
       "Радиальный обжимной пресс для изготовления и производства РВД",
     image: "https://stanok-rvd.ru/stanki_rvd/finnpower/p16hp_small.jpg",
-    link: "../finnpower/p16hp_p20hp/",
+    link: "finnpower/p16hp_p20hp/",
     specs: [
       { label: "Тип:", value: "Радиальный пресс" },
       { label: "Назначение:", value: "Ремонт шлангов" },
@@ -18,7 +18,7 @@ const finnpowerProductsData = [
     description:
       "Радиальный обжимной пресс для профессионального использования",
     image: "https://stanok-rvd.ru/stanki_rvd/finnpower/p20hp_small.jpg",
-    link: "../finnpower/press_p20hp/",
+    link: "finnpower/press_p20hp/",
     specs: [
       { label: "Тип:", value: "Радиальный пресс" },
       { label: "Назначение:", value: "Ремонт шлангов" },
@@ -29,7 +29,7 @@ const finnpowerProductsData = [
     name: "ОБЖИМНОЙ СТАНОК FINN-POWER P20X",
     description: "Оборудование для изготовления и производства РВД",
     image: "https://stanok-rvd.ru/stanki_rvd/finnpower/p20x_small.jpg",
-    link: "../finnpower/p21ms_p32x/",
+    link: "finnpower/p21ms_p32x/",
     specs: [
       { label: "Тип:", value: "Радиальный пресс" },
       { label: "Назначение:", value: "Производство РВД" },
@@ -41,7 +41,7 @@ const finnpowerProductsData = [
     description:
       "Радиальный обжимной пресс для изготовления и производства РВД",
     image: "https://stanok-rvd.ru/stanki_rvd/finnpower/p20nms_small.jpg",
-    link: "../finnpower/p20nms/",
+    link: "finnpower/p20nms/",
     specs: [
       { label: "Тип:", value: "Радиальный пресс" },
       { label: "Назначение:", value: "Ремонт шлангов" },
@@ -52,7 +52,7 @@ const finnpowerProductsData = [
     name: "ОБЖИМНОЙ СТАНОК FINN-POWER P20MS",
     description: "Радиальный обжимной пресс для профессионального производства",
     image: "https://stanok-rvd.ru/stanki_rvd/finnpower/p20ms_small.jpg",
-    link: "../finnpower/20ms_32ms/",
+    link: "finnpower/20ms_32ms/",
     specs: [
       { label: "Тип:", value: "Радиальный пресс" },
       { label: "Назначение:", value: "Ремонт шлангов" },
@@ -63,7 +63,7 @@ const finnpowerProductsData = [
     name: "ОБЖИМНОЙ СТАНОК FINN-POWER P32X",
     description: "Оборудование для изготовления и производства РВД",
     image: "https://stanok-rvd.ru/stanki_rvd/finnpower/p32x_small.jpg",
-    link: "../finnpower/press_p32x/",
+    link: "finnpower/press_p32x/",
     specs: [
       { label: "Тип:", value: "Радиальный пресс" },
       { label: "Назначение:", value: "Производство РВД" },
@@ -75,7 +75,7 @@ const finnpowerProductsData = [
     description:
       "Радиальный обжимной пресс для профессионального использования",
     image: "https://stanok-rvd.ru/stanki_rvd/finnpower/p32nms_small.jpg",
-    link: "../finnpower/p32nms/",
+    link: "finnpower/p32nms/",
     specs: [
       { label: "Тип:", value: "Радиальный пресс" },
       { label: "Назначение:", value: "Ремонт шлангов" },
@@ -86,7 +86,7 @@ const finnpowerProductsData = [
     name: "ОБЖИМНОЙ СТАНОК FINN-POWER P32MS",
     description: "Радиальный обжимной пресс для серийного производства",
     image: "https://stanok-rvd.ru/stanki_rvd/finnpower/p32ms_small.jpg",
-    link: "../finnpower/press_p32ms/",
+    link: "finnpower/press_p32ms/",
     specs: [
       { label: "Тип:", value: "Радиальный пресс" },
       { label: "Назначение:", value: "Ремонт шлангов" },
@@ -97,7 +97,7 @@ const finnpowerProductsData = [
     name: "ОБЖИМНОЙ СТАНОК FINN-POWER P51UC",
     description: "Гидравлические фитинги Interlock. Радиальный обжимной пресс",
     image: "https://stanok-rvd.ru/stanki_rvd/finnpower/p51uc_small.jpg",
-    link: "../finnpower/51uc/",
+    link: "finnpower/51uc/",
     specs: [
       { label: "Тип:", value: "Радиальный пресс" },
       { label: "Назначение:", value: "Мастерские для сервиса" },
@@ -108,7 +108,7 @@ const finnpowerProductsData = [
     name: "ОБЖИМНОЙ СТАНОК FINN-POWER P60UC",
     description: "Гидравлические фитинги Interlock. Радиальный обжимной пресс",
     image: "https://stanok-rvd.ru/stanki_rvd/finnpower/p60uc_small.jpg",
-    link: "../finnpower/p60uc/",
+    link: "finnpower/p60uc/",
     specs: [
       { label: "Тип:", value: "Радиальный пресс" },
       { label: "Назначение:", value: "Мобильные мастерские" },
@@ -120,7 +120,7 @@ const finnpowerProductsData = [
     description:
       "Пресс для серийного производства. Мастерские для сервиса шлангов",
     image: "https://stanok-rvd.ru/stanki_rvd/finnpower/120uc_small.jpg",
-    link: "../finnpower/fp20_fp120/",
+    link: "finnpower/fp20_fp120/",
     specs: [
       { label: "Тип:", value: "Пресс серийный" },
       { label: "Назначение:", value: "Сервис шлангов" },
@@ -132,7 +132,7 @@ const finnpowerProductsData = [
     description:
       "Пресс для серийного производства. Гидравлические фитинги Interlock",
     image: "https://stanok-rvd.ru/stanki_rvd/finnpower/120uc_small.jpg",
-    link: "../finnpower/fp120/",
+    link: "finnpower/fp120/",
     specs: [
       { label: "Тип:", value: "Пресс серийный" },
       { label: "Назначение:", value: "Сервис шлангов" },
